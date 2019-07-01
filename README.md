0d01aca940b7faaa8c6676709761612871acbe82
Devine Lu Linvega <aliceffekt@gmail.com>
2019-07-01 17:29:16 +0900

    *

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # linn
-Linnstrument layout on Monome
+
+This tiny console script will create a new virtual instrument called **Linn Monome**, from your **Monome 128**.
+
+## Setup
+
+```
+npm install
+node start
+```
+
+It will create a new midi instrument called `Linn Monome`.
+
+## Layout
+
+```
+F3   F3#  G3   G3#  A3   A3# B3   C4   C4#  D4   D4#  E4   F4  F4#  G4   G4#
+C3   C3#  D3   D3#  E3   F3  F3#  G3   G3#  A3   A3#  B3   C4  C4#  D4   D4#
+G2   G2#  A2   A2#  B2   C3  C3#  D3   D3#  E3   F3   F3#  G3  G3#  A3   A3#
+D2   D2#  E2   F2   F2#  G2  G2#  A2   A2#  B2   C3   C3#  D3  D3#  E3   F3
+A1   A1#  B1   C3   C2#  D2  D2#  E2   F2   F2#  G2   G2#  A2  A2#  B2   C3
+E1   F1   F1#  G1   G1#  A1  A1#  B1   C2   C2#  D2   D2#  E2  F2   F2#  G2
+B0   C1   C1#  D1   D1#  E1  F1   F1#  G1   G1#  A1   A1#  B1  C2   C2#  D2
+F0#  G0   G0#  A0   A0#  B0  C1   C1#  D1   E1   F1   F1#  G1  G1#  A1   A1#
+```
+
+Enjoy!
--- index.js
+++ index.js
@@ -7,8 +7,27 @@ serialosc.start()
 
 let grid = null
 
+let keys = ['C', 'c', 'D', 'd', 'E', 'F', 'f', 'G', 'g', 'A', 'a', 'B']
+
+let notes = [
+  'F3', 'F3#', 'G3', 'G3#', 'A3', 'A3#', 'B3', 'C4', 'C4#', 'D4', 'D4#', 'E4', 'F4', 'F4#', 'G4', 'G4#',
+  'C3', 'C3#', 'D3', 'D3#', 'E3', 'F3', 'F3#', 'G3', 'G3#', 'A3', 'A3#', 'B3', 'C4', 'C4#', 'D4', 'D4#',
+  'G2', 'G2#', 'A2', 'A2#', 'B2', 'C3', 'C3#', 'D3', 'D3#', 'E3', 'F3', 'F3#', 'G3', 'G3#', 'A3', 'A3#',
+  'D2', 'D2#', 'E2', 'F2', 'F2#', 'G2', 'G2#', 'A2', 'A2#', 'B2', 'C3', 'C3#', 'D3', 'D3#', 'E3', 'F3',
+  'A1', 'A1#', 'B1', 'C3', 'C2#', 'D2', 'D2#', 'E2', 'F2', 'F2#', 'G2', 'G2#', 'A2', 'A2#', 'B2', 'C3',
+  'E1', 'F1', 'F1#', 'G1', 'G1#', 'A1', 'A1#', 'B1', 'C2', 'C2#', 'D2', 'D2#', 'E2', 'F2', 'F2#', 'G2',
+  'B0', 'C1', 'C1#', 'D1', 'D1#', 'E1', 'F1', 'F1#', 'G1', 'G1#', 'A1', 'A1#', 'B1', 'C2', 'C2#', 'D2',
+  'F0#', 'G0', 'G0#', 'A0', 'A0#', 'B0', 'C1', 'C1#', 'D1', 'E1', 'F1', 'F1#', 'G1', 'G1#', 'A1', 'A1#'
+]
+
 function noteAt (i) {
-  return { n: 'C', l: 0 }
+  const n = notes[i]
+  const k = n.substr(0, 1)
+  const o = parseInt(n.substr(1, 1)) + 2
+  const s = n.indexOf('#') > -1
+  const l = s ? 0 : k === 'C' ? 15 : 5
+  const v = keys.indexOf(k) + (o * 12)
+  return { i, k, o, s, l, v }
 }
 
 function posAt (i) {
@@ -23,7 +42,7 @@ function initDevice () {
   for (let i = 0; i < 128; i++) {
     const pos = posAt(i)
     const light = noteAt(i).l
-    grid.set(pos.x, pos.y, light)
+    grid.levelSet(pos.x, pos.y, light)
   }
 }
 
@@ -41,14 +60,14 @@ function onKey (data) {
 
 function onKeyDown (x, y) {
   const note = noteAt(idAt(x, y))
-  console.log(note)
-  grid.set(x, y, 1)
-  output.send('noteon', { note: 64, velocity: 127, channel: 3 })
+  grid.levelSet(x, y, 10)
+  output.send('noteon', { note: note.v, velocity: 127, channel: 3 })
 }
 
 function onKeyUp (x, y) {
-  grid.set(x, y, 0)
-  output.send('noteon', { note: 64, velocity: 127, channel: 3 })
+  const note = noteAt(idAt(x, y))
+  grid.levelSet(x, y, note.l)
+  output.send('noteoff', { note: note.v, velocity: 127, channel: 3 })
 }
 
 serialosc.on('device:add', selectDevice)
