d1db2e5d8ecd15db4834c97ce3706a2b89e82ac3
Devine Lu Linvega <aliceffekt@gmail.com>
2019-07-02 07:16:49 +0900

    Cleanup

--- index.js
+++ index.js
@@ -1,25 +1,14 @@
 
 const name = 'Linn Monome'
-
 const readline = require('readline')
 const easymidi = require('easymidi')
 const serialosc = require('serialosc')
-
-console.log(`Creating ${name}..`)
-
 const output = new easymidi.Output(name, true)
-
-console.log(`Starting serialosc..`)
-
-serialosc.start()
-
-console.log('Waiting..')
-console.log('Press any key to stop.')
-
-//
 
 let channel = 1
 let grid = null
+let last = null
+let down = 0
 let fn = false
 
 const keys = ['C', 'c', 'D', 'd', 'E', 'F', 'f', 'G', 'g', 'A', 'a', 'B']
@@ -34,6 +23,10 @@ const notes = [
   'B0', 'C1', 'C1#', 'D1', 'D1#', 'E1', 'F1', 'F1#', 'G1', 'G1#', 'A1', 'A1#', 'B1', 'C2', 'C2#', 'D2',
   'F0#', 'G0', 'G0#', 'A0', 'A0#', 'B0', 'C1', 'C1#', 'D1', 'D1#', 'E1', 'F1', 'F1#', 'G1', 'G1#', 'A1'
 ]
+
+serialosc.start()
+
+// Utils
 
 function noteAt (i) {
   const n = notes[i]
@@ -54,43 +47,42 @@ function idAt (x, y) {
   return (y * 16) + x
 }
 
-function initDevice () {
+function redraw () {
   for (let i = 0; i < 128; i++) {
     const pos = posAt(i)
-    const light = noteAt(i).l
-    grid.levelSet(pos.x, pos.y, light)
+    const note = noteAt(i)
+    grid.levelSet(pos.x, pos.y, last && note.p === last ? 10 : note.l)
   }
-  console.log('Ready.')
 }
 
-function selectDevice (device) {
-  grid = device
-  grid.all(0)
-  grid.on('key', onKey)
-  console.log(`Selecting ${device.model}(${device.sizeX}x${device.sizeY}) ${device.id}..`)
-  initDevice()
-}
-
-function onKey (data) {
-  if (data.s === 1) { onKeyDown(data.x, data.y) } else { onKeyUp(data.x, data.y) }
+function print (note) {
+  console.log(`${(note.i + '').padStart(3, ' ')} | ${(note.p + '').padStart(3, ' ')} | ${(note.v + '').padStart(3, ' ')} | ch:${(channel + '').padStart(3, ' ')}`)
 }
 
 function onKeyDown (x, y) {
   const note = noteAt(idAt(x, y))
+  down += 1
   grid.levelSet(x, y, 10)
   output.send('noteon', { note: note.v, velocity: 127, channel: channel })
-  console.log(`${(note.i + '').padStart(3, ' ')} | ${(note.p + '').padStart(3, ' ')} | ${(note.v + '').padStart(3, ' ')} | ch:${(channel + '').padStart(3, ' ')}`)
+  print(note)
   if (fn && idAt(x, y) < 16) {
     channel = idAt(x, y)
   }
   fn = note.i === 127
+  last = note.p
+  redraw()
 }
 
 function onKeyUp (x, y) {
   const note = noteAt(idAt(x, y))
+  down -= 1
   grid.levelSet(x, y, note.l)
   output.send('noteoff', { note: note.v, velocity: 127, channel: channel })
   fn = false
+  if (down < 1) {
+    last = null
+  }
+  redraw()
 }
 
 function close () {
@@ -100,7 +92,13 @@ function close () {
 
 // Ready
 
-serialosc.on('device:add', selectDevice)
+serialosc.on('device:add', (device) => {
+  grid = device
+  grid.all(0)
+  grid.on('key', (data) => { if (data.s === 1) { onKeyDown(data.x, data.y) } else { onKeyUp(data.x, data.y) } })
+  console.log(`Selecting ${device.model}(${device.sizeX}x${device.sizeY}) ${device.id}..`)
+  redraw()
+})
 
 // Quit
 
